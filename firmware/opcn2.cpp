30dd0cfa69f45758c5c766dca1d05cdd83e0311a
David H Hagan <david@davidhhagan.com>
2016-03-05 15:02:10 -0500

    remove config

--- firmware/opcn2.cpp
+++ firmware/opcn2.cpp
@@ -283,6 +283,7 @@ hist_data Opcn2Library::OPCN2::histogram(){
     return data;
 }
 */
+/*
 OPCN2::ConfigurationVariables OPCN2::config(){
     // Read the config variables
     ConfigurationVariables results;       // empty structure for the data
@@ -389,6 +390,7 @@ OPCN2::ConfigurationVariables OPCN2::config(){
 
     return results;
 }
+*/
 
 String OPCN2::info_string(){
     // Read the info String and return the firmware version
